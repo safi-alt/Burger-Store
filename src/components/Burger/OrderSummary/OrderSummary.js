e35aab64560663c5c0d28c10c2d0f6d5a968e53e
safi <safiazim8@gmail.com>
2020-06-06 00:19:33 +0500

    Performance fixes with LifeCycle Hooks for temporary basis

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,43 @@
-import React from "react";
+// import React from "react";
+import React, { Component } from "react";
 import Auxiliary from "./../../../hoc/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKeys) => {
-    return (
-      <li key={igKeys}>
-        <span style={{ textTransform: "capitalize" }}>{igKeys}</span>:
-        {props.ingredients[igKeys]}
-      </li>
-    );
-  });
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Auxiliary>
-  );
-};
+class OrderSummary extends Component {
+  // This could be a functional component because it is now improved in performance by the modal componenet but it is remained a class component because as refrence
+  componentWillUpdate() {
+    console.log("[OrderSummary] WillUpdate");
+  }
 
-export default orderSummary;
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKeys) => {
+        return (
+          <li key={igKeys}>
+            <span style={{ textTransform: "capitalize" }}>{igKeys}</span>:
+            {this.props.ingredients[igKeys]}
+          </li>
+        );
+      }
+    );
+    return (
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Auxiliary>
+    );
+  }
+}
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,34 @@
-import React from "react";
+// import React from "react";
+import React, { Component } from "react";
 import Auxiliary from "../../../hoc/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from "./Modal.css";
 
-const modal = (props) => {
-  return (
-    <Auxiliary>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0",
-        }}
-      >
-        {props.children}
-      </div>
-    </Auxiliary>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentWillUpdate() {
+    console.log("[Modal] WillUpdate");
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Auxiliary>
+    );
+  }
+}
+
+export default Modal;
