b4c9913a22cee1d7f2d7321ffddb23b0f7f900f8
safi <safiazim8@gmail.com>
2020-07-02 20:18:05 +0500

    Passing data VIA query Params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,6 +11,16 @@ class Checkout extends Component {
     },
   };
 
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      //['salad','1']
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients });
+  }
+
   checkoutCancelHandler = () => {
     this.props.history.goBack();
   };
