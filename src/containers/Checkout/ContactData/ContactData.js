fc44b2db521b16c4c90346ae1c67e5779dc6d543
safi <safiazim8@gmail.com>
2020-07-08 02:27:21 +0500

    Working on a error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ class ContactData extends Component {
             { value: "cheapest", displayValue: "Cheapest" },
           ],
         },
+        validation: {},
         value: "",
         valid: true,
       },
@@ -125,6 +126,10 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+
+    if (!rules) {
+      return true;
+    }
 
     if (rules.required) {
       isValid = value.trim() !== "" && isValid;
