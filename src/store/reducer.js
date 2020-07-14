f56bff23f74a6d12ad822fab5952e5efed852b64
safi <safiazim8@gmail.com>
2020-07-14 16:42:13 +0500

    Setting up redux and react redux

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4,
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
