b5113653f74e2fe82e3082735b00f61e1d27603f
safi <safiazim8@gmail.com>
2020-07-16 22:21:36 +0500

    Implementing Redux Thunk and Middleware

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,15 +18,15 @@ class BurgerBuilder extends Component {
     error: false,
   };
   componentDidMount() {
-    // console.log(this.props);
-    // axios
-    //   .get("https://react-my-burger-8990f.firebaseio.com/ingredients.json")
-    //   .then((response) => {
-    //     this.setState({ ingredients: response.data });
-    //   })
-    //   .catch((error) => {
-    //     this.setState({ error: true });
-    //   });
+    console.log(this.props);
+    axios
+      .get("https://react-my-burger-8990f.firebaseio.com/ingredients.json")
+      .then((response) => {
+        this.setState({ ingredients: response.data });
+      })
+      .catch((error) => {
+        this.setState({ error: true });
+      });
   }
 
   updatePurchaseState(ingredients) {
