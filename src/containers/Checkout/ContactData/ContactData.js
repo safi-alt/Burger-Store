51bda6751dc03b9c4aa8b096e7d3866a670f8ad5
safi <safiazim8@gmail.com>
2020-07-18 22:48:27 +0500

    Adding Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -113,16 +113,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData,
     };
-
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        this.setState({ loading: false });
-        this.props.history.push("/");
-      })
-      .catch((error) => {
-        this.setState({ loading: false });
-      });
   };
 
   checkValidity(value, rules) {
