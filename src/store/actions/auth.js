872d02f09aef5b99bed926482bf4106ca40c5f9d
safi <safiazim8@gmail.com>
2020-07-25 00:00:03 +0500

    Creating Logging out action and making adjustable to expire time

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = (error) => {
   };
 };
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignUp) => {
   return (dispatch) => {
     dispatch(authStart());
@@ -41,6 +55,7 @@ export const auth = (email, password, isSignUp) => {
       .then((response) => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch((err) => {
         dispatch(authFail(err.response.data.error));
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -28,6 +28,10 @@ const authFail = (state, action) => {
   });
 };
 
+const authLogout = (state, action) => {
+  return updateObject(state, { token: null, userId: null });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
@@ -38,6 +42,9 @@ const reducer = (state = initialState, action) => {
 
     case actionTypes.AUTH_FAIL:
       return authFail(state, action);
+
+    case actionTypes.AUTH_LOGOUT:
+      return authLogout(state, action);
 
     default:
       return state;
