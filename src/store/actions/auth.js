1daffbba74401a628a41dc9ee6a14dea853bbc1a
safi <safiazim8@gmail.com>
2020-07-24 17:27:45 +0500

    Adding Sign-In

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 export const authStart = () => {
   return {
@@ -15,13 +16,31 @@ export const authSuccess = (authData) => {
 
 export const authFail = (error) => {
   return {
-    type: actionTypes.FETCH_ORDERS_FAIL,
+    type: actionTypes.AUTH_FAIL,
     error: error,
   };
 };
 
-export const auth = (username, password) => {
+export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyByiKyzmiz_jT-X_2k0wuD6ZYn0piiUGEc",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
   };
 };
