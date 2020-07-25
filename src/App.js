bc08eba49acba7bb06f9f6260a3844d4ed7193e4
safi <safiazim8@gmail.com>
2020-07-25 23:42:12 +0500

    Adding a Logout Link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
 import { Route, Switch } from "react-router-dom";
 import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/Logout/Logout";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../../store/actions/index";
+import { Redirect } from "react-router-dom";
+
+class Logout extends Component {
+  componentDidMount() {
+    this.props.onLogout();
+  }
+
+  render() {
+    return <Redirect to="/" />;
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLogout: () => dispatch(actions.logout()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Logout);
