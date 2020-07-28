84268ccf30df0bd525d9e7dadc6575ecd8415c14
safi <safiazim8@gmail.com>
2020-07-29 03:25:30 +0500

    Playing with jest and enzyme methods

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -22,4 +22,11 @@ describe("<NavigatioItems />", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it("should render three <NavigationItems /> elements if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
