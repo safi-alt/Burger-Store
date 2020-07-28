43936b7356342069af4077d7d1196f2e8f0f5aa9
safi <safiazim8@gmail.com>
2020-07-29 03:37:30 +0500

    Testing components correctly

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,7 +23,7 @@ describe("<NavigatioItems />", () => {
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 
-  it("should render three <NavigationItems /> elements if authenticated", () => {
+  it("should render an exact logout button", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(
       wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
